094e0c2b4d74aa4df612799810a22f7353abb643
TanuiDev <bk.kibiwott@gmail.com>
2026-07-26 21:16:27 +0300

    Updated

---
+++ src/features/auth/usersAPI.ts
@@ -0,0 +1,43 @@
+import { ApiUrl } from "../../utils/apiURL";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { RootState } from "../../app/store";
+
+export type Tuser = {
+  id: number;
+  userName: string;
+  emailAddress: string;
+  password: string;
+  phoneNumber: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: ApiUrl,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).user.accesstoken;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+        headers.set("Content-Type", "application/json");
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["User"],
+  endpoints: (builder) => ({
+    createUser: builder.mutation<Tuser, Partial<Tuser>>({
+      query: (body) => ({
+        url: "/auth/register",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["User"],
+   },
+    ),
+  }),
+});
+
+export const { useCreateUserMutation } = userApi;
